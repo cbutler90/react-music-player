2ece3e6c26175b052cc1e0772f46ca6e1092ad98
Christine Butler <37420934+cbutler90@users.noreply.github.com>
2022-10-02 23:55:29 -0400

    music is playinggit add . able to play/pause cards

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,10 +1,18 @@
+import { useDispatch, useSelector } from 'react-redux';
+
 import { Error, Loader, SongCard } from '../components';
 import { genres } from '../assets/constants';
 import { useGetTopChartsQuery } from '../redux/services/shazamCore';
 
 const Discover = () => {
+  const dispatch = useDispatch();
+  const { activeSong, isPlaying } = useSelector((state) => state.player);
   const { data, isFetching, error } = useGetTopChartsQuery();
   const genreTitle = 'Pop';
+
+  if(isFetching) return <Loader title="Loading songs..." />;
+
+  if(error) return <Error />;
 
   console.log(data);
 
@@ -17,10 +25,13 @@ const Discover = () => {
         </select>
       </div>
       <div className="flex flex-wrap sm:justify-start justify-center gap-8">
-        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((song, i) => (
+        {data?.map((song, i) => (
           <SongCard 
             key={song.key}
             song={song}
+            isPlaying={isPlaying}
+            activeSong={activeSong}
+            data={data}
             i={i}
           />
         ))}
